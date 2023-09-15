3beb0e31db53d0077620f9ee5dd4822221957bce
D <67240388+imitelis@users.noreply.github.com>
2023-09-14 23:03:44 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,145 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// canvas settings
+ctx.fillStyle = 'white';
+ctx.lineWidth = 1;
+
+class Particle {
+	constructor(effect){
+		this.effect = effect;
+		this.x = Math.floor(Math.random() * this.effect.width);
+		this.y = Math.floor(Math.random() * this.effect.height);
+		this.speedX;
+		this.speedY;
+		this.speedModifier = Math.floor(Math.random() * 5 + 1);
+		this.history = [{ x: this.x, y: this.y }];
+		this.maxLength = Math.floor(Math.random() * 200 + 10);
+		this.angle = 0;
+		this.timer = this.maxLength * 2;
+		this.colors = [ '#ff4400', '#ff7f50', '#ff8f66', '#ffb499', '#ffdacc' ];
+		this.color = this.colors[Math.floor(Math.random() * this.colors.length)];
+	}
+	draw(context){
+		context.beginPath();
+		context.moveTo(this.history[0].x, this.history[0].y);
+		for (let i = 0; i < this.history.length; i++){
+			context.lineTo(this.history[i].x, this.history[i].y);
+		}
+		context.strokeStyle = this.color;
+		context.stroke();
+	}
+	update(){
+		this.timer--;
+		if (this.timer >= 1) {
+			let x = Math.floor(this.x / this.effect.cellSize);
+			let y = Math.floor(this.y / this.effect.cellSize);
+			let index = y * this.effect.cols + x;
+			this.angle = this.effect.flowField[index];
+
+			this.speedX = Math.cos(this.angle);
+			this.speedY = Math.sin(this.angle);
+			this.x += this.speedX * this.speedModifier;
+			this.y += this.speedY * this.speedModifier;
+
+			this.history.push({ x: this.x, y: this.y });
+			if (this.history.length > this.maxLength) {
+				this.history.shift();
+			}
+		} else if (this.history.length > 1) {
+			this.history.shift();
+		} else {
+			this.reset();
+		}
+	}
+	reset(){
+		this.x = Math.floor(Math.random() * this.effect.width);
+		this.y = Math.floor(Math.random() * this.effect.height);
+		this.history = [{x: this.x, y: this.y}];
+		this.timer = this.maxLength * 2;
+	}
+}
+
+class Effect {
+	constructor(canvas){
+		this.canvas = canvas;
+		this.width = this.canvas.width;
+		this.height = this.canvas.height;
+		this.particles = [];
+		this.numberOfParticles = this.canvas.width * 2;
+		this.cellSize = 20;
+		this.rows;
+		this.cols;
+		this.flowField = [];
+		this.curve = 2.2;
+		this.zoom = 0.15;
+		this.init();
+	}
+
+	init(){
+		// create flow field
+		this.rows = Math.floor(this.height / this.cellSize);
+		this.cols = Math.floor(this.width / this.cellSize);
+		this.flowField = [];
+		for (let y = 0; y < this.rows; y++) {
+			for (let x = 0; x < this.cols; x++) {
+				let angle = (Math.cos(x * this.zoom) + Math.sin(y * this.zoom)) * this.curve;
+				this.flowField.push(angle);
+			}
+			// console.log(this.flowField)
+		}
+
+		// create particles
+		this.particles = [];
+		for (let i = 0; i < this.numberOfParticles; i++) {
+			this.particles.push(new Particle(this));
+		}
+	}
+	drawGrid(context){
+		for (let c = 0; c < this.cols; c++) {
+			context.beginPath();
+			context.moveTo(this.cellSize * c, 0);
+			context.lineTo(this.cellSize * c, this.height);
+			context.strokeStyle = 'rgba(255, 255, 255, 0.5)';
+			context.stroke();
+		}
+		for (let r = 0; r < this.rows; r++) {
+			context.beginPath();
+			context.moveTo(0, this.cellSize * r);
+			context.lineTo(this.width, this.cellSize * r);
+			context.strokeStyle = 'rgba(255, 255, 255, 0.5)';
+			context.stroke();
+		}
+	}
+	resize(width, height) {
+		this.canvas.width = width;
+		this.canvas.height = height;
+		this.width = this.canvas.width;
+		this.height = this.canvas.height;
+		this.init();
+	}
+	render(context){
+		this.drawGrid(context);
+		this.particles.forEach(particle => {
+			particle.draw(context);
+			particle.update();
+		})
+	}
+}
+
+const effect = new Effect(canvas);
+
+function animate(){
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	effect.render(ctx);
+	requestAnimationFrame(animate);
+}
+
+animate();
+
+window.addEventListener('resize',
+  function () {
+    effect.render(ctx);
+})
